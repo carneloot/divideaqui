a19ab420db13e388302b3386ac3e4637ddf5fc3b
Matheus Carnelutt <carneloot@proton.me>
2025-11-03 01:58:34 -0300

    fix: adding buffer polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,17 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
+			buffer: 'buffer',
+		},
+	},
+	define: {
+		global: 'globalThis',
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: 'globalThis',
+			},
 		},
 	},
 })
